a861aba6dcec9f2c5b3486e3fb6454d860d61705
Alberto Melendez <alberto.melendez@afas.nl>
2018-12-29 22:03:20 +0100

    Added header style

--- ts-src/flow/pageLoader.ts
+++ ts-src/flow/pageLoader.ts
@@ -1,17 +1,19 @@
+import $ from 'jquery';
+
 export class Header {
     Load() {
-        console.log("Loading the page header");
-    }    
+        $("#header").html("<div class='header'><h3>Ameblog Sample Application</h3></div>");
+    }
 }
 
 export class Body {
     Load() {
         console.log("Loading the page body");
-    }    
+    }
 }
 
 export class Footer {
     Load() {
         console.log("Loading the page footer");
-    }    
+    }
 }
