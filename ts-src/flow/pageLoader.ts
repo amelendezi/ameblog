42b371764f7042eed484c0f1baf9de05519b281c
Alberto Melendez <alberto.melendez@afas.nl>
2018-12-29 05:46:38 +0100

    Added folder

---
+++ ts-src/flow/pageLoader.ts
@@ -0,0 +1,17 @@
+export class Header {
+    Load() {
+        console.log("Loading the page header");
+    }    
+}
+
+export class Body {
+    Load() {
+        console.log("Loading the page body");
+    }    
+}
+
+export class Footer {
+    Load() {
+        console.log("Loading the page footer");
+    }    
+}
